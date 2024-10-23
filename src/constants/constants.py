450843342fcfdffbf2910ef35aaf118fe4db0a87
shreyash2106 <shreyash2106@berkeley.edu>
2024-10-23 11:02:53 -0400

    rename code to src and add dictionary mapping from group number to mood type

---
+++ src/constants/constants.py
@@ -0,0 +1,9 @@
+group_mood_mapping = {
+    1: "Sad/Acoustic",
+    2: "Happy/Danceable",
+    3: "Energetic",
+    4: "Calm/Instrumental",
+    5: "Mixed Mood",
+    6: "Energetic/High Tempo",
+    7: "Calm/Relaxing"
+}
